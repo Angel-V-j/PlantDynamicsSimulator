45838391d67f0573b854bd98e6ec059e27cb2135
Angel-V-j <stu2201681028@uni-plovdiv.bg>
2026-06-08 03:00:33 +0300

    Weather system in progress

--- weather/WeatherSystem.cpp
+++ weather/WeatherSystem.cpp
@@ -1,1 +1,80 @@
 #include "WeatherSystem.h"
+
+WeatherSystem::WeatherSystem() {
+}
+
+WeatherSystem::WeatherSystem(double cloudiness, double wind) {
+    this->cloudiness = cloudiness;
+    this->cloudinessTarget = cloudiness;
+    this->wind = wind;
+    this->windTarget = wind;
+    this->rainChance = 0;
+    this->rainIntensity = 0;
+    this->isRaining = false;
+}
+
+void WeatherSystem::update(const double & chaos) {
+    updateCloudinessTarget(chaos, 20);
+    updateWindTarget(chaos, 25);
+    updateRainChance(chaos, 15);
+    updateRainState(chaos);
+    updateRainIntensity(chaos);
+}
+
+void WeatherSystem::tick(const double & delta, const double & chaos) {
+
+
+    for (int i = 0; i < 60; i++) {
+        moveToTarget(cloudinessTarget, this->cloudiness, delta);
+        moveToTarget(windTarget, this->wind, delta);
+
+    }
+}
+
+void WeatherSystem::moveToTarget(const double & target, double & currentValue, const double speed) {
+    currentValue += (target - currentValue) * speed;
+}
+
+double WeatherSystem::getCloudiness() const {
+    return this->cloudiness;
+}
+
+double WeatherSystem::getCloudinessTarget() const {
+    return this->cloudinessTarget;
+}
+
+double WeatherSystem::getWind() const {
+    return this->wind;
+}
+
+double WeatherSystem::getWindTarget() const {
+    return this->windTarget;
+}
+
+double WeatherSystem::getRainChance() const {
+    return this->rainChance;
+}
+
+double WeatherSystem::getRainIntensity() const {
+    return rainIntensity;
+}
+
+bool WeatherSystem::getIsRaining() const {
+    return isRaining;
+}
+
+void WeatherSystem::updateCloudinessTarget(const double chaos, const float multiplayer) {
+    this ->cloudinessTarget += (chaos - 0.5) * multiplayer;
+}
+void WeatherSystem::updateWindTarget(const double chaos, const float multiplayer) {
+    this->windTarget += (chaos - 0.5) * multiplayer;
+}
+void WeatherSystem::updateRainChance(const double chaos, const float multiplayer) {
+    this->windTarget += (chaos - 0.5) * multiplayer;
+}
+void WeatherSystem::updateRainState(const double chaos) {
+    if (cloudiness < 50 && chaos < 0.5)
+        this->isRaining = false;
+    else
+        this->isRaining = true;
+}
--- weather/WeatherSystem.h
+++ weather/WeatherSystem.h
@@ -1,12 +1,41 @@
-//
-// Created by Angel on 6/7/2026.
-//
-
 #ifndef PLANTDYNAMICSSIMULATOR_WEATHERSYSTEM_H
 #define PLANTDYNAMICSSIMULATOR_WEATHERSYSTEM_H
 
-
 class WeatherSystem {
+    private:
+        double cloudiness;
+        double cloudinessTarget;
+
+        double wind;
+        double windTarget;
+
+        double rainChance;
+        double rainIntensity;
+        bool isRaining;
+
+        void tick(const double & delta);
+        void moveToTarget(const double & target, double & currentValue, double speed);
+    public:
+        WeatherSystem();
+        WeatherSystem(double cloudiness, double wind);
+
+        void update();
+
+        double getCloudiness() const;
+        double getCloudinessTarget() const;
+        double getWind() const;
+        double getWindTarget() const;
+        double getRainChance() const;
+        double getRainIntensity() const;
+        bool getIsRaining() const;
+
+        void updateCloudinessTarget(double chaos, float multiplayer);
+        void updateWindTarget(double chaos, float multiplayer);
+        void updateRainChance(double chaos, float multiplayer);
+        void updateRainIntensity(double chaos, float multiplayer);
+        void updateRainState(double chaos);
+
+        ~WeatherSystem() = default;
 };
 
 
